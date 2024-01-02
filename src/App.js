8d4c1c0a041ad9b286de1f9ad6d47adefda4ea73
Omolola <adefilaomolola@gmail.com>
2024-01-02 15:57:20 -0800

    Adder footer which includes a link to my Github

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Weather App
-        
-    
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Omolola Adeojo and is {"  "}
+        <a
+          href="https://github.com/omolola-A/react-weather-app.git"
+          rel="noreferrer" target="_blank"
+        > 
+          open-source on Github
+        </a>
+      </footer>
     </div>
   );
 }
