135aa8ab49a604cbff67904ff55388c44c10c54c
Omolola <adefilaomolola@gmail.com>
2024-01-04 17:27:45 -0800

    Added autofocus to form

--- src/Weather.js
+++ src/Weather.js
@@ -11,6 +11,7 @@ export default function Weather() {
                 type="search"
                 placeholder="Enter a city.."
                 className="form-control"
+                autoFocus="on"
               />
             </div>
             <div className="col-3">
