b827c80b9acdffc0c3bca18c396204305e0c3132
Omolola <adefilaomolola@gmail.com>
2024-01-05 22:48:51 -0800

    Added the component FormattedDate, and didplayed day and time through it

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import axios from "axios";
+import FormattedDate from "./FormattedDate";
 import "./Weather.css";
 
 export default function Weather(props) {
@@ -11,7 +12,7 @@ export default function Weather(props) {
       temperature: response.data.main.temp,
       city: response.data.name,
       wind: response.data.wind.speed,
-      date: "Wednesday 07:00",
+      date: new Date(response.data.dt*1000),
       description: response.data.weather[0].description,
       iconUrl:"https://ssl.gstatic.com/onebox/weather/64/rain_s_cloudy.png",
       humidity: response.data.main.humidity 
@@ -43,7 +44,9 @@ return (
         </form>
         <h1>{weatherData.city}</h1>
         <ul>
-          <li>{weatherData.date}</li>
+          <li>
+            <FormattedDate date= {weatherData.date}/>
+            </li>
           <li className="text-capitalize">{weatherData.description}</li>
         </ul>
         <div className="row mt-3">
