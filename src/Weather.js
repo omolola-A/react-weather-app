84eadc8bb9189c91e808cbf4d28e9a9335465fec
Omolola <adefilaomolola@gmail.com>
2024-01-11 11:45:20 -0800

    Intall and import react-animated-weather

--- src/Weather.js
+++ src/Weather.js
@@ -16,7 +16,7 @@ export default function Weather(props) {
       wind: response.data.wind.speed,
       date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
-      iconUrl:  `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      icon: response.data.weather[0].icon,
       humidity: response.data.main.humidity,
     });
   
