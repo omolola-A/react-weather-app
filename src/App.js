ece1c286999af80217ad1d72b3439a3294df80e3
Omolola <adefilaomolola@gmail.com>
2024-01-21 16:14:22 -0800

    Added my portfolio link

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import Weather from './Weather';
 function App() {
   return (
     <div className="App">
-      <div className="container">
-      </div>
-      <Weather defaultCity= "New York"/>
+      <div className="container"></div>
+      <Weather defaultCity="New York" />
       <footer>
-        This project was coded by Omolola Adeojo and is {"  "}
+        This project was coded by{" "}
+        <a href="https://potfolio-project.vercel.app/" rel="noreferrer" target="_blank" >Omolola Adeojo </a> and
+        is {"  "}
         <a
           href="https://github.com/omolola-A/react-weather-app.git"
-          rel="noreferrer" target="_blank"
-        > 
+          rel="noreferrer"
+          target="_blank"
+        >
           open-source on Github
         </a>
       </footer>
